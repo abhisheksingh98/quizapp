3a28257458a8a3ace91c3f8ce58ff278a8d58422
abhisheksingh98 <writetoabhishek07@gmail.com>
2022-07-19 19:09:30 +0530

    Feat - Add Question component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import CategorySelector from "./components/CategorySelector";
+import Question from "./components/Question";
 
 import "./App.css";
 import useTdb from "./hooks/useTdb";
 
 export default function App() {
-  const { category, setCategory } = useTdb();
+  const { question, getQuestion, category, setCategory } = useTdb();
 
   return (
     <div className="app">
       <div className="question-header">
         <CategorySelector category={category} chooseCategory={setCategory} />
+      </div>
+      <div className="question-main">
+        {question && (
+          <Question
+            question={question}
+            answerQuestion={handleQuestionAnswered}
+          />
+        )}
       </div>
     </div>
   );
